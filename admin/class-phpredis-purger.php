365518dcd0c113a151581e8083a010eac54a5472
Raymond Walker <raymond.walker@greenpeace.org>
2019-03-20 13:37:03 +1100

    Fallback to $parse['host'] if not behind a proxy

--- admin/class-phpredis-purger.php
+++ admin/class-phpredis-purger.php
@@ -72,7 +72,8 @@ class PhpRedis_Purger extends Purger {
 
 			$parse             = wp_parse_url( get_home_url() );
 			$parse['path']     = empty( $parse['path'] ) ? '/' : $parse['path'];
-			$total_keys_purged = $this->delete_keys_by_wildcard( $prefix . $parse['scheme'] . 'GET' . $_SERVER['HTTP_X_FORWARDED_HOST'] . $parse['path'] . '*' );
+			$host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $parse['host'];
+			$total_keys_purged = $this->delete_keys_by_wildcard( $prefix . $parse['scheme'] . 'GET' . $host . $parse['path'] . '*' );
 			$this->log( '* ' . get_home_url() . ' Purged! * ' );
 
 		}
@@ -118,9 +119,10 @@ class PhpRedis_Purger extends Purger {
 			$parse['path'] = '';
 		}
 
+		$host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $parse['host'];
+
 		$prefix          = $nginx_helper_admin->options['redis_prefix'];
-		$_url_purge_base = $prefix . $parse['scheme'] . 'GET' . $_SERVER['HTTP_X_FORWARDED_HOST'] . $parse['path'];
-		$is_purged       = $this->delete_single_key( $_url_purge_base );
+		$_url_purge_base = $prefix . $parse['scheme'] . 'GET' . $host . $parse['path'];
 
 		/**
 		 * To delete device type caches such as `<URL>--mobile`, `<URL>--desktop`, `<URL>--lowend`, etc.
@@ -172,7 +174,8 @@ class PhpRedis_Purger extends Purger {
 
 		$parse           = wp_parse_url( home_url() );
 		$prefix          = $nginx_helper_admin->options['redis_prefix'];
-		$_url_purge_base = $prefix . $parse['scheme'] . 'GET' . $_SERVER['HTTP_X_FORWARDED_HOST'];
+		$host = isset($_SERVER['HTTP_X_FORWARDED_HOST']) ? $_SERVER['HTTP_X_FORWARDED_HOST'] : $parse['host'];
+		$_url_purge_base = $prefix . $parse['scheme'] . 'GET' . $host;
 
 		$purge_urls = isset( $nginx_helper_admin->options['purge_url'] ) && ! empty( $nginx_helper_admin->options['purge_url'] ) ?
 			explode( "\r\n", $nginx_helper_admin->options['purge_url'] ) : array();
